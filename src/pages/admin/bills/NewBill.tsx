12d69fb792c70830cb922354886bfe5479209ab1
locnt10022001 <thanhloc10022001@gmail.com>
2024-10-21 22:02:16 +0700

    New file
    Update file

---
+++ src/pages/admin/bills/NewBill.tsx
@@ -0,0 +1,154 @@
+import { useEffect, useState } from 'react';
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons"
+import { Link } from 'react-router-dom';
+import { GetAllBill, RemoveBill } from '../../../services/bill';
+import IBill from '../../../types/bill';
+import ListItemsOrder from '../../../components/ListItemsOrder';
+import {
+  Button,
+  Cascader,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Select,
+  Switch,
+  Table,
+  TreeSelect,
+} from 'antd';
+import React from 'react';
+import { Space, Tag } from 'antd';
+import type { TableProps } from 'antd';
+type SizeType = Parameters<typeof Form>[0]['size'];
+
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+  tags: string[];
+}
+
+const columns: TableProps<DataType>['columns'] = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const data: DataType[] = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
+
+const NewBill = () => {
+  const [componentSize, setComponentSize] = useState<SizeType | 'default'>('default');
+
+  const onFormLayoutChange = ({ size }: { size: SizeType }) => {
+    setComponentSize(size);
+  };
+
+  return (
+    <Form
+      labelCol={{ span: 4 }}
+      wrapperCol={{ span: 14 }}
+      layout="horizontal"
+      initialValues={{ size: componentSize }}
+      onValuesChange={onFormLayoutChange}
+      size={componentSize as SizeType}
+      style={{ maxWidth: 600 }}
+    >
+      <Form.Item label="Input">
+        <Input />
+      </Form.Item>
+      <Form.Item label="Select">
+        <Select>
+          <Select.Option value="demo">Demo</Select.Option>
+        </Select>
+      </Form.Item>
+      <Form.Item label="TreeSelect">
+        <TreeSelect
+          treeData={[
+            { title: 'Light', value: 'light', children: [{ title: 'Bamboo', value: 'bamboo' }] },
+          ]}
+        />
+      </Form.Item>
+      <Form.Item label="Nguoi ban"> "user.username"
+         </Form.Item>
+      <Form.Item label="Ngay Ban ">
+        <DatePicker />
+      </Form.Item>
+      <Table<DataType> columns={columns} dataSource={data} />;
+      <Form.Item label="Switch" valuePropName="checked">
+        <Switch />
+      </Form.Item>
+      <Form.Item label="Button">
+        <Button>Button</Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default NewBill
---
+++ src/pages/admin/bills/index.ts
@@ -0,0 +1,4 @@
+import ManageBill from "./ManageBill"
+import ManageBillUpdate from "./ManageBillUpdate"
+import NewBill from "./NewBill"
+export {ManageBill,ManageBillUpdate,NewBill}
